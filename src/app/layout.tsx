6dab05242435a2260f94f01d59755d33ffd889f2
Osscccar <Oscarvonbergen@gmail.com>
2025-05-05 06:23:51 +1000

    Read posthog docs instead

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Unbounded } from "next/font/google";
 import { GoogleTagManager } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { PostHogPageView } from "@/lib/posthog";
+import { PostHogProvider } from "./providers";
 import Script from "next/script";
 
 // Load Satoshi as a local font with all weights
@@ -85,30 +85,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          id="posthog-script"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init bs ws ge fs capture De Ai $s register register_once register_for_session unregister unregister_for_session Is getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSurveysLoaded onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey canRenderSurveyAsync identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty xs Ss createPersonProfile Es gs opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing ys debug ks getPageViewId captureTraceFeedback captureTraceMetric".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-            posthog.init('phc_deiCeICBs4UTwrHBbd0i3BjusZuSLSNKHcSiWIB8sPM', {
-              api_host: 'https://us.i.posthog.com',
-              person_profiles: 'identified_only',
-              capture_pageview: false // We'll handle this manually
-            })
-          `,
-          }}
-        />
-      </head>
       <body className={`${satoshi.variable} ${unbounded.variable}`}>
-        <GoogleTagManager gtmId="AW-17023467754" />
-        <FirebaseProvider>
-          <PostHogPageView /> {/* Add this component */}
-          {children}
-        </FirebaseProvider>
-        <Analytics />
-        <SpeedInsights />
+        <PostHogProvider>
+          <GoogleTagManager gtmId="AW-17023467754" />
+          <FirebaseProvider>
+            <PostHogPageView /> {/* Add this component */}
+            {children}
+          </FirebaseProvider>
+          <Analytics />
+          <SpeedInsights />
+        </PostHogProvider>
       </body>
     </html>
   );
